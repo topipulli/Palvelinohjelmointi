6c320cadd560036b2d268ded1c442bebbcc61257
topipulli <topipulli@localhost>
2022-08-29 21:32:08 +0300

    first commit

---
+++ Location/src/main/java/LocationHello/Location/web/locationController.java
@@ -0,0 +1,22 @@
+package LocationHello.Location.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class locationController {
+	
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String sayHelloToUser(@RequestParam(name="name", required=false, defaultValue="John Doe") String name, 
+			@RequestParam(name="location", required=false, defaultValue = "Nowhere") String location) {
+		
+		return "Welcome to " + location + " " + name + "!";
+		
+	}
+	
+	}
+
+
---
+++ Location/src/test/java/LocationHello/Location/LocationApplicationTests.java
@@ -0,0 +1,13 @@
+package LocationHello.Location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
